5b7b0e37689c9bdba89d896b2b4ea46ab9688e19
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-11 13:24:03 +0500

    create code_1.0

---
+++ HomeWork_1_seminar_8/Program.cs
@@ -0,0 +1,50 @@
+﻿Random rnd = new Random();
+int m = rnd.Next(4,6);
+int n = rnd.Next(4,6);
+
+int[,] array = new int[m,n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = rnd.Next(-99,99);
+        System.Console.Write(String.Format("{0,3}", array[i,j])+ " ");
+    }
+    System.Console.WriteLine();
+}
+
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(String.Format("{0,3}", array[i,j])+ " ");
+    }
+    Console.WriteLine();
+  }
+}
