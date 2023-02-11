0019a9169fbdf69060d77854e62b3bd810c2b143
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-11 14:15:14 +0500

    create code_2.0

---
+++ HomeWork_2_seminar_8/Program.cs
@@ -0,0 +1,37 @@
+﻿Random rnd = new Random();
+int m = rnd.Next(5, 7);
+int n = rnd.Next(3, 4);
+
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = rnd.Next(-99, 99);
+        System.Console.Write(String.Format("{0,3}", array[i, j]) + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int sum = int.MaxValue;
+
+int index = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int temp = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        temp += array[i, j];
+    }
+    if (temp < sum)
+    {
+        sum = temp;
+        index = i;
+    }
+}
+
+Console.WriteLine("Строка с индексем: " + index + " Сумма: " + sum);
+Console.WriteLine();
+
