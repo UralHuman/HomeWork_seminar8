38bdf4634c49d4557bfd432bb19a402eafcf262b
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-11 15:40:41 +0500

    create code_3.0

---
+++ HomeWork_3_seminar_8/Program.cs
@@ -0,0 +1,64 @@
+﻿Random rnd = new Random();
+
+int m = rnd.Next(4, 6);
+
+int[,] matrix1 = new int[m, m];
+
+System.Console.WriteLine("\nMатрица № 1");
+System.Console.WriteLine();
+
+for (int i = 0; i < matrix1.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix1.GetLength(1); j++)
+    {
+        matrix1[i, j] = rnd.Next(1, 99);
+        System.Console.Write(String.Format("{0,3}", matrix1[i, j]) + " ");
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.WriteLine("\nMатрица № 2");
+System.Console.WriteLine();
+
+int[,] matrix2 = new int[m, m];
+
+for (int i = 0; i < matrix2.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        matrix2[i, j] = rnd.Next(1, 99);
+        System.Console.Write(String.Format("{0,3}", matrix2[i, j]) + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int[,] matrix3 = new int[m, m];
+
+System.Console.WriteLine("\nРезультирующая матрица:");
+System.Console.WriteLine();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        matrix1[i, j] = matrix1[i, j] * matrix2[i, j];
+        matrix3[i, j] = matrix1[i, j];
+
+
+    }
+}
+WriteArray(matrix3);
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,5}", array[i, j]) + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
